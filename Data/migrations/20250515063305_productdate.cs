8593194b535d99a293f1591cdd195b18960e3866
MusaKavak <musa0kavak@gmail.com>
2025-05-15 10:20:53 +0300

    Migrations (Unuutum)

---
+++ Data/migrations/20250515063305_productdate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Data.migrations
+{
+    /// <inheritdoc />
+    public partial class productdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+        }
+    }
+}
